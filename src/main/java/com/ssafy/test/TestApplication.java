aa1be1add3974fec9a3d7d827f2abc04c06b4207
fftl <fftl0785@gmail.com>
2023-01-20 13:25:39 +0900

    update : Dockerfile

--- src/main/java/com/ssafy/test/TestApplication.java
+++ src/main/java/com/ssafy/test/TestApplication.java
@@ -9,7 +9,7 @@ public class TestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestApplication.class, args);
-		System.out.println("gkgkgkgkgkgkgk");
+		System.out.println("야옹야옹");
 	}
 
 }
