b892251e3fa56c9685aae5dde0bb2b1cece07bf3
이동민 <fftl0785@gmail.com>
2023-01-22 17:53:59 +0900

    update : jsoup, test code

--- src/main/java/com/ssafy/test/TestApplication.java
+++ src/main/java/com/ssafy/test/TestApplication.java
@@ -1,15 +1,19 @@
 package com.ssafy.test;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class TestApplication {
-
-
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(TestApplication.class, args);
-		System.out.println("야옹야옹");
-	}
+		System.out.println("뭐지?");
+		String crawlingURL = "https://news.naver.com/main/ranking/popularDay.naver";
 
+		Document document = Jsoup.connect(crawlingURL).get();
+		System.out.println(document.select(".list_title"));
+	}
 }
