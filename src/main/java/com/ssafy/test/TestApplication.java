45524be4928638032f7b5a24a82fef955ad1e057
이동민 <fftl0785@gmail.com>
2023-01-23 12:45:49 +0900

    update: jsoup test

--- src/main/java/com/ssafy/test/TestApplication.java
+++ src/main/java/com/ssafy/test/TestApplication.java
@@ -10,10 +10,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TestApplication {
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(TestApplication.class, args);
-		System.out.println("뭐지?");
+		//간단하게 그냥 main 메소드에서 테스트 진행하였습니다.
+		//gradle에서 jsoup을 추가 한 뒤 진행합니다.
+
+		//내가 크롤링 하고자 하는 사이트의 링크를 담은 crawlingURL 입니다.
 		String crawlingURL = "https://news.naver.com/main/ranking/popularDay.naver";
 
+		//해당 URL을 담은 Jsoup document를 만듭니다.
 		Document document = Jsoup.connect(crawlingURL).get();
+
+		//document에서 select함수를 이용해 .list_title 클래스를 가진 모든 항목을 가져왔습니다!
 		System.out.println(document.select(".list_title"));
 	}
 }
