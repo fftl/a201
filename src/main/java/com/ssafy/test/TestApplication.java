89b80a7ef00d911a01b68d33552ecfa5a34ffdc1
이동민 <fftl0785@gmail.com>
2023-01-24 17:31:32 +0900

    update : index.jsp

--- src/main/java/com/ssafy/test/TestApplication.java
+++ src/main/java/com/ssafy/test/TestApplication.java
@@ -14,12 +14,12 @@ public class TestApplication {
 		//gradle에서 jsoup을 추가 한 뒤 진행합니다.
 
 		//내가 크롤링 하고자 하는 사이트의 링크를 담은 crawlingURL 입니다.
-		String crawlingURL = "https://news.naver.com/main/ranking/popularDay.naver";
-
-		//해당 URL을 담은 Jsoup document를 만듭니다.
-		Document document = Jsoup.connect(crawlingURL).get();
-
-		//document에서 select함수를 이용해 .list_title 클래스를 가진 모든 항목을 가져왔습니다!
-		System.out.println(document.select(".list_title"));
+//		String crawlingURL = "https://news.naver.com/main/ranking/popularDay.naver";
+//
+//		//해당 URL을 담은 Jsoup document를 만듭니다.
+//		Document document = Jsoup.connect(crawlingURL).get();
+//
+//		//document에서 select함수를 이용해 .list_title 클래스를 가진 모든 항목을 가져왔습니다!
+//		System.out.println(document.select(".list_title"));
 	}
 }
