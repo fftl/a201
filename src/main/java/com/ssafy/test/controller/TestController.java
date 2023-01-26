bc43e0166e0490396da5daf11aa19de4e4f4ae92
fftl <fftl0785@gmail.com>
2023-01-26 12:31:00 +0900

    update: test api 생성

---
+++ src/main/java/com/ssafy/test/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.ssafy.test.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RequestMapping("/test")
+@RestController
+public class TestController {
+
+    @GetMapping
+    public String getTest(){
+        System.out.println("test");
+       return "getTest 통신성공!";
+    }
+    @PostMapp                                                                                                                                          ing
+    public String postTest(@RequestParam("name") String name){
+        return "postTest 통신성공! name =>" + name ;
+    }
+}
